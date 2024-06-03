0d35be8f3d100bdf77373ceda3482c8c0ba7b831
Takashi Suwa <bd.gfngfn@gmail.com>
2024-06-03 14:19:01 +0900

    make `transpose` use matrix sizes

--- src/Lwsd/Matrix.hs
+++ src/Lwsd/Matrix.hs
@@ -1,8 +1,8 @@
 module Lwsd.Matrix
   ( Matrix,
     ConstructionError (..),
-    fromList,
-    toList,
+    fromRows,
+    toRows,
     size,
     transpose,
     mult,
@@ -15,59 +15,57 @@ import Prelude
 newtype Matrix = Matrix [[Int]]
   deriving newtype (Eq, Show)
 
-data ConstructionError = ConstructionError
-  { row1 :: [Int],
-    row2 :: [Int]
-  }
+data ConstructionError
+  = EmptyRow
+  | InconsistencyOfRowLength [Int] [Int]
 
-fromList :: [[Int]] -> Either ConstructionError Matrix
-fromList [] = pure $ Matrix []
-fromList rows@(firstRow : restRows) =
-  case List.find (\row -> List.length row /= expectedLength) restRows of
-    Just row' -> Left $ ConstructionError firstRow row'
-    Nothing -> pure $ Matrix rows
+fromRows :: [[Int]] -> Either ConstructionError Matrix
+fromRows [] = pure $ Matrix []
+fromRows rows@(firstRow : restRows) =
+  if expectedLength == 0
+    then Left EmptyRow -- Matrices of size (0, n) is not allowed where n > 0
+    else
+      case List.find (\row -> List.length row /= expectedLength) restRows of
+        Just row' -> Left $ InconsistencyOfRowLength firstRow row'
+        Nothing -> pure $ Matrix rows
   where
     expectedLength = List.length firstRow
 
-toList :: Matrix -> [[Int]]
-toList (Matrix rows) = rows
+toRows :: Matrix -> [[Int]]
+toRows (Matrix rows) = rows
 
 size :: Matrix -> (Int, Int)
 size (Matrix []) = (0, 0)
 size (Matrix rows@(firstRow : _)) = (List.length rows, List.length firstRow)
 
-transpose :: Matrix -> Matrix
-transpose (Matrix []) = Matrix []
-transpose (Matrix (firstRow : restRows)) =
-  Matrix $ reverse $ go [] firstRow restRows
+transpose :: Int -> Int -> Matrix -> Maybe Matrix
+transpose 0 0 (Matrix []) = pure $ Matrix []
+transpose m n _ | m <= 0 || n <= 0 = Nothing
+transpose m n (Matrix rows) | m == List.length rows =
+  Matrix <$> go [] n rows
   where
-    go acc [] restRows0 =
-      if List.all null restRows0
-        then acc
-        else error "transpose 1"
-    go acc (firstElem : restElems) restRows0 =
-      let pairs =
-            map
-              ( \case
-                  [] -> error "transpose 2"
-                  firstElem0 : restElems0 -> (firstElem0, restElems0)
-              )
-              restRows0
-          column = firstElem : map fst pairs
-          restRows1 = map snd pairs
-       in go (column : acc) restElems restRows1
+    go :: [[Int]] -> Int -> [[Int]] -> Maybe [[Int]]
+    go _ k _ | k < 0 = Nothing
+    go acc 0 rows0 =
+      if List.all null rows0
+        then pure $ reverse acc
+        else Nothing
+    go acc k rows0 = do
+      firstAndRestPairs <- mapM List.uncons rows0
+      let column = map fst firstAndRestPairs
+      let rows1 = map snd firstAndRestPairs
+      go (column : acc) (k - 1) rows1
+transpose _ _ _ = Nothing
 
 mult :: Int -> Int -> Int -> Matrix -> Matrix -> Maybe Matrix
-mult k m n mat1 mat2 =
+mult k m n mat1 mat2 = do
+  let Matrix rows1 = mat1 -- `rows1`: a list of length k
+  Matrix columns2 <- transpose m n mat2 -- `columns2`: a list of length n
   if size mat1 == (k, m) || size mat2 == (m, n)
   -- `mat1`: a matrix of size k × m
   -- `mat2`: a matrix of size m × n
-    then Just $ Matrix [ [ calc row1 column2 | row1 <- rows1 ] | column2 <- columns2 ]
+    then pure $ Matrix [ [ calc row1 column2 | row1 <- rows1 ] | column2 <- columns2 ]
     else Nothing
   where
-    Matrix rows1 = mat1 -- `rows1`: a list of length k
-    Matrix columns2 = transpose mat2 -- `columns2`: a list of length n
-
     calc :: [Int] -> [Int] -> Int
-    calc row column =
-      List.foldl' (+) 0 $ zipWith (*) row column
+    calc row column = List.foldl' (+) 0 $ zipWith (*) row column
--- test/MatrixSpec.hs
+++ test/MatrixSpec.hs
@@ -6,7 +6,7 @@ import Test.Hspec
 
 make :: [[Int]] -> Matrix
 make rows =
-  case Matrix.fromList rows of
+  case Matrix.fromRows rows of
     Left _ -> error "make"
     Right mat -> mat
 
@@ -14,16 +14,23 @@ spec :: Spec
 spec = do
   describe "Matrix.transpose" $ do
     it "computes transpositions (1)" $
-      Matrix.transpose (make [[1, 2], [3, 4]])
-        `shouldBe` make [[1, 3], [2, 4]]
+      Matrix.transpose 2 2 (make [[1, 2], [3, 4]])
+        `shouldBe` Just (make [[1, 3], [2, 4]])
     it "computes transpositions (2)" $
-      Matrix.transpose (make [[1, 2, 3], [4, 5, 6]])
-        `shouldBe` make [[1, 4], [2, 5], [3, 6]]
+      Matrix.transpose 2 3 (make [[1, 2, 3], [4, 5, 6]])
+        `shouldBe` Just (make [[1, 4], [2, 5], [3, 6]])
   describe "Matrix.mult" $ do
-    it "computes multiplications" $
+    it "computes multiplications (1)" $
       Matrix.mult 2 3 1
         (make [[1, 2, 3], [4, 5, 6]])
         (make [[8], [9], [10]])
           `shouldBe` Just (make [[56, 137]])
     -- 1*8 + 2*9 + 3*10 = 56
     -- 4*8 + 5*9 + 6*10 = 137
+    it "computes multiplications (2)" $
+      Matrix.mult 2 3 2
+        (make [[1, 2, 3], [4, 5, 6]])
+        (make [[8, 0], [9, 11], [10, 7]])
+          `shouldBe` Just (make [[56, 137], [43, 97]])
+    -- 1*0 + 2*11 + 3*7 = 43
+    -- 4*0 + 5*11 + 6*7 = 97
