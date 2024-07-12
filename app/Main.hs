163ec02c68c9f22c4267df85d8b3ea5b3b40c590
Takashi Suwa <bd.gfngfn@gmail.com>
2024-07-12 19:32:30 +0900

    change exit codes for failures, and add option `--compile-time-only`

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Lwsd.LibMain
 import Options.Applicative
+import System.Exit
 
 argumentParser :: Parser Argument
 argumentParser =
@@ -9,6 +10,13 @@ argumentParser =
     <$> strArgument (metavar "STRING")
     <*> switch (short 'o' <> long "optimize" <> help "Do slight optimization about assertion insertion")
     <*> option auto (value 80 <> short 'w' <> long "diplay-width" <> help "Set display width")
+    <*> switch (long "compile-time-only" <> help "Execute only compile-time computation")
 
 main :: IO ()
-main = execParser (info argumentParser briefDesc) >>= handle
+main = do
+  arg <- execParser (info argumentParser briefDesc)
+  wasSuccess <- handle arg
+  if wasSuccess then
+    exitSuccess
+  else
+    exitFailure
