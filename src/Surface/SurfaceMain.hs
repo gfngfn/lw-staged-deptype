847fb4ffec4be75269d86ca9013352e810d278ef
Takashi Suwa <bd.gfngfn@gmail.com>
2024-10-06 20:27:34 +0900

    add `mat.surf`

--- src/Surface/SurfaceMain.hs
+++ src/Surface/SurfaceMain.hs
@@ -1,6 +1,11 @@
-module Surface.SurfaceMain where
+module Surface.SurfaceMain
+  ( Argument (..),
+    handle,
+  )
+where
 
 import Data.Text.IO qualified as TextIO
+import Surface.Parser qualified as Parser
 import Prelude
 
 newtype Argument = Argument
@@ -10,5 +15,15 @@ newtype Argument = Argument
 handle :: Argument -> IO Bool
 handle Argument {inputFilePath} = do
   putStrLn "Lightweight Dependent Types via Staging (Surface Language)"
-  _source <- TextIO.readFile inputFilePath
-  return True -- TODO: implement parsing and binding-time analysis
+  source <- TextIO.readFile inputFilePath
+  case Parser.parseExpr source of
+    Left err -> do
+      putStrLn "-------- parse error: --------"
+      putStrLn err
+      failure
+    Right e -> do
+      print e
+      success
+  where
+    success = return True
+    failure = return False
--- src/Surface/Syntax.hs
+++ src/Surface/Syntax.hs
@@ -23,6 +23,7 @@ data Literal
   deriving stock (Eq, Show)
 
 data ExprF ann = Expr ann (ExprMain ann)
+  deriving stock (Show)
 
 data ExprMain ann
   = Literal Literal
@@ -30,15 +31,18 @@ data ExprMain ann
   | Lam (Var, TypeExprF ann) (ExprF ann)
   | App (ExprF ann) (ExprF ann)
   | LetIn Var (ExprF ann) (ExprF ann)
+  deriving stock (Show)
 
 type Expr = ExprF Span
 
 type TypeName = Text
 
 data TypeExprF ann = TypeExpr ann (TypeExprMain ann)
+  deriving stock (Show)
 
 data TypeExprMain ann
   = TyName TypeName [ExprF ann]
   | TyArrow (Maybe Var, TypeExprF ann) (TypeExprF ann)
+  deriving stock (Show)
 
 type TypeExpr = TypeExprF Span
