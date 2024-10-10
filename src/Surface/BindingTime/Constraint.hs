ff4749350484f6ebc92e51065c0ed911985ae512
Takashi Suwa <bd.gfngfn@gmail.com>
2024-10-10 19:21:16 +0900

    slightly optimize the constraint solving algorithm about `Subst`

--- src/Surface/BindingTime/Constraint.hs
+++ src/Surface/BindingTime/Constraint.hs
@@ -19,7 +19,7 @@ type BindingTimeSubst = Map BindingTimeVar BindingTime
 
 data SolvingStepResult ann
   = NotFound
-  | Subst BindingTimeVar BindingTime
+  | Subst BindingTimeVar BindingTime [Constraint ann]
   | TrivialEliminated [Constraint ann]
   | ContradictionDetected ann
 
@@ -36,9 +36,9 @@ solveConstraints = go Map.empty
       case step [] constraints of
         NotFound ->
           pure (accMap, constraints)
-        Subst btvFrom btTo -> do
+        Subst btvFrom btTo remainingConstraints -> do
           let accMapNew = Map.insert btvFrom btTo (substSubst btvFrom btTo accMap)
-          let constraintsNew = map (substConstraint btvFrom btTo) constraints
+          let constraintsNew = map (substConstraint btvFrom btTo) remainingConstraints
           go accMapNew constraintsNew
         TrivialEliminated constraintsNew ->
           go accMap constraintsNew
@@ -49,19 +49,19 @@ solveConstraints = go Map.empty
     step constraintAcc = \case
       [] -> NotFound
       CLeq _ann (BTVar btv1) (BTVar btv2) : rest | btv1 == btv2 -> TrivialEliminated (reverse constraintAcc ++ rest)
-      CLeq _ann (BTVar btv1) (BTConst BT0) : _ -> Subst btv1 (BTConst BT0)
-      CLeq _ann (BTConst BT1) (BTVar btv2) : _ -> Subst btv2 (BTConst BT1)
+      CLeq _ann (BTVar btv1) (BTConst BT0) : rest -> Subst btv1 (BTConst BT0) (reverse constraintAcc ++ rest)
+      CLeq _ann (BTConst BT1) (BTVar btv2) : rest -> Subst btv2 (BTConst BT1) (reverse constraintAcc ++ rest)
       CLeq _ann (BTConst BT0) _ : rest -> TrivialEliminated (reverse constraintAcc ++ rest)
       CLeq _ann _ (BTConst BT1) : rest -> TrivialEliminated (reverse constraintAcc ++ rest)
       CLeq ann (BTConst BT1) (BTConst BT0) : _ -> ContradictionDetected ann
+      constraint@(CLeq _ann (BTVar _) (BTVar _)) : rest -> step (constraint : constraintAcc) rest
       CEqual _ann (BTVar btv1) (BTVar btv2) : rest | btv1 == btv2 -> TrivialEliminated (reverse constraintAcc ++ rest)
-      CEqual _ann (BTVar btv1) bt2 : _ -> Subst btv1 bt2
-      CEqual _ann bt1 (BTVar btv2) : _ -> Subst btv2 bt1
+      CEqual _ann (BTVar btv1) bt2 : rest -> Subst btv1 bt2 (reverse constraintAcc ++ rest)
+      CEqual _ann bt1 (BTVar btv2) : rest -> Subst btv2 bt1 (reverse constraintAcc ++ rest)
       CEqual ann (BTConst btc1) (BTConst btc2) : rest ->
         if btc1 == btc2
           then TrivialEliminated (reverse constraintAcc ++ rest)
           else ContradictionDetected ann
-      constraint : rest -> step (constraint : constraintAcc) rest
 
     substSubst :: BindingTimeVar -> BindingTime -> BindingTimeSubst -> BindingTimeSubst
     substSubst btvFrom btTo =
